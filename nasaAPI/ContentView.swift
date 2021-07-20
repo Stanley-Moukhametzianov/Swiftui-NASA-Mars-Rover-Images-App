52baee0aef3da9ffeceee4824d2ee9dd9d0283b5
Stanley Moukhametzianov <Stanleymoukh@gmail.com>
2021-07-20 16:25:25 -0400

    Initial Commit

---
+++ nasaAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nasaAPI
+//
+//  Created by Stanley Moukh on 7/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
